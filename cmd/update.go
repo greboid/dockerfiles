42598366250a6763b05d9dd94ce1cf7892bdd1cf
Greg Holmes <git@greg.holmes.name>
2021-09-06 18:49:59 +0100

    Add irc-webhook to updates

--- cmd/update.go
+++ cmd/update.go
@@ -51,9 +51,9 @@ func main() {
 		"irc-github",       // depends on golang + base
 		"irc-goplum",       // depends on golang + base
 		"irc-news",         // depends on golang + base
+                "irc-webhook",      // depends on golang + base
 		"linx-server",      // depends on golang + base
 		"postgres-13",      // depends on alpine
-		// "vault",        // depends on alpine + golang
 	}
 
 	for i := range targets {
