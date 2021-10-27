a993d82f9abbf10a38d034eef89813d95c97843b
Greg Holmes <git@greg.holmes.name>
2021-10-27 13:43:11 +0100

    Actually mirror rather than debug

--- .github/mirror.sh
+++ .github/mirror.sh
@@ -7,8 +7,7 @@ buildah login -u $REPO_OWNER -p $GITHUB_TOKEN $MIRROR_TARGET
 
 for IMAGE in $MIRROR_IMAGES; do
   if buildah images $REGISTRY/$IMAGE >/dev/null 2>&1; then
-    echo $IMAGE changed, mirroring
-  else
-    echo $IMAGE not changed
+    buildah tag $REGISTRY/$IMAGE $MIRROR_TARGET/$MIRROR_PATH/$IMAGE
+    buildah push $MIRROR_TARGET/$MIRROR_PATH/$IMAGE
   fi
 done
