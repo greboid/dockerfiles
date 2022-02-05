be2403c803a08314b0fac300effeb1cc441a70c9
Greg Holmes <git@greg.holmes.name>
2022-02-05 23:27:31 +0000

    Readd needed credentials, change name so contempt doens't pick them up

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -5,7 +5,7 @@ set -eux
 go install github.com/csmith/contempt/cmd/contempt@latest
 git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
-buildah login -u $REGISTRY_USER -p $REGISTRY_PASS $REGISTRY
+buildah login -u $REG_USER -p $REG_PASS $REGISTRY
 buildah login -u $REPO_OWNER -p $GITHUB_TOKEN $MIRROR_TARGET
 contempt --commit --build --push . .
 git push
