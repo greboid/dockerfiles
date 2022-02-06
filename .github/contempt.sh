61462ed03718c20c54217324cb05a72fa9bf1293
greboid <git@greg.holmes.name>
2022-02-06 12:47:54 +0000

    Change name of auth token

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -6,6 +6,6 @@ go install github.com/csmith/contempt/cmd/contempt@latest
 git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
 buildah login -u $REG_USER -p $REG_PASS $REGISTRY
-buildah login -u $REPO_OWNER -p $GITHUB_TOKEN $MIRROR_TARGET
+buildah login -u $REPO_OWNER -p $CONTEMPT_TOKEN $MIRROR_TARGET
 contempt --commit --build --push . .
 git push
