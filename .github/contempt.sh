7522d03633e2c921860f4dc7f470e01b7cb76934
Greg Holmes <greboid@users.noreply.github.com>
2022-02-07 14:30:54 +0000

    Debugging

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -3,12 +3,13 @@
 set -eux
 
 rm $HOME/.docker/config.json || true
+mkdir -p /home/runner/work/xdg
+export XDG_RUNTIME_DIR=/home/runner/work/xdg
 
 go install github.com/csmith/contempt/cmd/contempt@latest
 git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
 buildah login -u $REG_USER -p $REG_PASS $REGISTRY
 buildah login -u $REPO_OWNER -p $CONTEMPT_TOKEN $MIRROR_TARGET
-ls /run/containers/$UID
 contempt --commit --build --push . .
 git push
