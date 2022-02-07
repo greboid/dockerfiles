059e85d3a6075e22dc1e6c626c2ea949206d86bb
Greg Holmes <greboid@users.noreply.github.com>
2022-02-07 14:07:33 +0000

    Debugging

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -9,6 +9,7 @@ git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
 buildah login -u $REG_USER -p $REG_PASS $REGISTRY
 buildah login -u $REPO_OWNER -p $CONTEMPT_TOKEN $MIRROR_TARGET
-buildah pull ghcr.io/greboid/cv
+ls "$XDG_RUNTIME_DIR/containers/auth.json"
+env
 contempt --commit --build --push . .
 git push
