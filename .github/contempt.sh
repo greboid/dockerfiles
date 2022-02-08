9a47624d47502a48da43c1be0a268f356e58ade4
Greg Holmes <greboid@users.noreply.github.com>
2022-02-08 18:30:35 +0000

    Remove docker login

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -5,7 +5,5 @@ set -eux
 go install github.com/csmith/contempt/cmd/contempt@latest
 git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
-docker login -u $REG_USER -p $REG_PASS $REGISTRY
-docker login $MIRROR_TARGET -u $REPO_OWNER -p $CONTEMPT_TOKEN 
 contempt --commit --build --push . .
 git push
