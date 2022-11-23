17cfb28ae4fd0433aae7dc092bd8bdff91fd5a98
Greg Holmes <git@greg.holmes.name>
2022-11-23 23:53:42 +0000

    Some improvements to postgres images

--- postgres-14/entry.sh
+++ postgres-14/entry.sh
@@ -17,12 +17,15 @@ fi
 if [ -z "$POSTGRES_DB" ]; then
   POSTGRES_DB=$PGUSER
 fi
+if [ -n "$POSTGRES_HOST_AUTH_METHOD" ]; then
+  POSTGRES_HOST_AUTH_METHOD="scram-sha-256"
+fi
 if [ ! -s "$PGDATA" ]; then
-        initdb -U postgres --pwfile=<(echo "$PGPASSWORD") -D $PGDATA --no-instructions --auth-host scram-sha-256 --auth-local trust
+        initdb -U postgres --pwfile=<(echo "$PGPASSWORD") -D $PGDATA --no-instructions --auth-host ${POSTGRES_HOST_AUTH_METHOD} --auth-local trust
         echo "local all all trust" > "$PGDATA/pg_hba.conf"
         echo "host all all 127.0.0.1/32 trust" >> "$PGDATA/pg_hba.conf"
         echo "host all all ::1/128 trust" >> "$PGDATA/pg_hba.conf"
-        echo "host all all all scram-sha-256" >> "$PGDATA/pg_hba.conf"
+        echo "host all all all ${POSTGRES_HOST_AUTH_METHOD}" >> "$PGDATA/pg_hba.conf"
 	pg_ctl -D "$PGDATA" -o "-c listen_addresses='127.0.0.1'" -o "-p 5432" -o "-c unix_socket_directories='/var/run/psql'" -w start
 	echo "CREATE USER ${PGUSER} WITH PASSWORD '${PGPASSWORD}';CREATE DATABASE ${POSTGRES_DB} OWNER '${PGUSER}';" | psql -U postgres
 	pg_ctl -D "$PGDATA" -m fast -w stop
