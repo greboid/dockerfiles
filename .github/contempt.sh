46c3f6937834ca3b3c215bb3b27a09962ca4d2bf
Greg Holmes <git@greg.holmes.name>
2023-06-03 21:12:37 +0100

    Switch to containerfiles

--- .github/contempt.sh
+++ .github/contempt.sh
@@ -5,5 +5,5 @@ set -eux
 go install github.com/csmith/contempt/cmd/contempt@latest
 git config user.name "$GIT_USERNAME"
 git config user.email "$GIT_EMAIL"
-contempt --commit --build --push . .
+contempt -template Containerfile.gotpl -output Containerfile --commit --build --push . .
 git push
