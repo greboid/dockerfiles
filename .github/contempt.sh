ddbf92331c78dc39f8d505ca84908666dcbbe9aa
greboid <git@greg.holmes.name>
2023-07-23 12:52:29 +0100

    Try new workflow

--- .github/contempt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-set -eux
-
-go install github.com/csmith/contempt/cmd/contempt@latest
-git config user.name "$GIT_USERNAME"
-git config user.email "$GIT_EMAIL"
-contempt -template Containerfile.gotpl -output Containerfile --commit --build --push . .
-git push
